df1d6f2a2b2b346794516e01e15f889ebbc99270
irvinghu07 <rhu5@dons.usfca.edu>
2018-12-24 18:28:23 +0800

    Runtime: 6 ms, faster than 40.30% of Java online submissions for Reshape the Matrix.

---
+++ src/ReshapeTheMatrix.java
@@ -0,0 +1,66 @@
+/**
+ * Development IDE: IntelliJ IDEA
+ * Author: irving
+ * Project Name: leetcode-Array
+ * Date: 2018-12-24
+ */
+
+/**
+ * In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
+ * <p>
+ * You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
+ * <p>
+ * The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+ * <p>
+ * If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+ * <p>
+ * Example 1:
+ * Input:
+ * nums =
+ * [[1,2],
+ * [3,4]]
+ * r = 1, c = 4
+ * Output:
+ * [[1,2,3,4]]
+ * Explanation:
+ * The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+ * Example 2:
+ * Input:
+ * nums =
+ * [[1,2],
+ * [3,4]]
+ * r = 2, c = 4
+ * Output:
+ * [[1,2],
+ * [3,4]]
+ * Explanation:
+ * There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+ * Note:
+ * The height and width of the given matrix is in range [1, 100].
+ * The given r and c are all positive.
+ */
+public class ReshapeTheMatrix {
+    public static void main(String[] args) {
+        int[][] a = {
+                {1, 2},
+                {3, 4}
+        };
+        System.out.println(3 / 4);
+        LeetCodeUtils.printArr(new ReshapeTheMatrix().matrixReshape(a, 2, 4));
+    }
+
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        if (nums.length*nums[0].length < r*c){
+            return nums;
+        }
+        int[][] ret = new int[r][c];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[0].length; j++) {
+                index = i * nums[0].length + j;
+                ret[index / c][index % c] = nums[i][j];
+            }
+        }
+        return ret;
+    }
+}
