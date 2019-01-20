a81624d4fd1f94154932f54d67afa0f0564eb67d
Irving Hu <31359459+irvinghu07@users.noreply.github.com>
2019-01-19 17:12:20 -0800

    Runtime: 9 ms, faster than 61.67% of Java online submissions for Unique Morse Code Words.

---
+++ src/string/easy/UniqueMorseCodeWords.java
@@ -0,0 +1,50 @@
+package string.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * {@see <a href="https://leetcode.com/problems/unique-morse-code-words/">instruction</a>}
+ * International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+ * <p>
+ * For convenience, the full table for the 26 letters of the English alphabet is given below:
+ * <p>
+ * [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+ * Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+ * <p>
+ * Return the number of different transformations among all words we have.
+ * <p>
+ * Example:
+ * Input: words = ["gin", "zen", "gig", "msg"]
+ * Output: 2
+ * Explanation:
+ * The transformation of each word is:
+ * "gin" -> "--...-."
+ * "zen" -> "--...-."
+ * "gig" -> "--...--."
+ * "msg" -> "--...--."
+ * <p>
+ * There are 2 different transformations, "--...-." and "--...--.".
+ * Note:
+ * <p>
+ * The length of words will be at most 100.
+ * Each words[i] will have length in range [1, 12].
+ * words[i] will only consist of lowercase letters.
+ */
+public class UniqueMorseCodeWords {
+
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] table = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        Set<String> container = new HashSet<>();
+        for (String word : words) {
+            StringBuffer stringBuffer = new StringBuffer();
+            char[] chars = word.toCharArray();
+            final int length = chars.length;
+            for (int i = 0; i < length; i++) {
+                stringBuffer.append(table[(int) chars[i] % 97]);
+            }
+            container.add(stringBuffer.toString());
+        }
+        return container.size();
+    }
+}
